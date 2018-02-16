f4aaaa2fd2b37ee3a53a3e5e76677c91bab5e2fc
marcomafcorp <marcomcse@gmail.com>
2018-02-15 23:18:20 -0800

    update

--- src/user.js
+++ src/user.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
   name: {
     type: String,
     validate: {
-      validator: ()  => ,
+      validator: (name) => name.length > 2,
       message: 'Name must be longer than 2 characters.'
     },
     required: [true, 'Name is required.']
--- test/validation_test.js
+++ test/validation_test.js
@@ -1,21 +1,22 @@
-const assert = require('assert')
-const User = require('../src/user')
 
-describe('Balidating records', () =>{
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Validating records', () => {
   it('requires a user name', () => {
     const user = new User({ name: undefined });
     const validationResult = user.validateSync();
-    const { message } = validationResult.console.errors.name;
+    const { message } = validationResult.errors.name;
 
-    assert(message === 'Name is required');
+    assert(message === 'Name is required.');
   });
 
-  it('requires a user\'s name longer than 2 characters', () =>{
+  it('requires a user\'s name longer than 2 characters', () => {
     const user = new User({ name: 'Al' });
     const validationResult = user.validateSync();
     const { message } = validationResult.errors.name;
 
-    assert(message === 'Name must be longer than 2 characters');
+    assert(message === 'Name must be longer than 2 characters.');
   });
 
   it('disallows invalid records from being saved', (done) => {
@@ -29,4 +30,3 @@ describe('Balidating records', () =>{
       });
   });
 });
-.62451420
